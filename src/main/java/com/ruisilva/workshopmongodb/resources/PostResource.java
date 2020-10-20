0fdbe83f37adb7c56c07d7cd77008c1bf85414c1
Rui Silva <ruisrsilva@gmail.com>
2020-10-20 19:47:32 +0100

    Simple query with query methods

--- src/main/java/com/ruisilva/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/ruisilva/workshopmongodb/resources/PostResource.java
@@ -3,10 +3,13 @@ package com.ruisilva.workshopmongodb.resources;
 import com.ruisilva.workshopmongodb.domain.Post;
 import com.ruisilva.workshopmongodb.domain.User;
 import com.ruisilva.workshopmongodb.dto.UserDTO;
+import com.ruisilva.workshopmongodb.resources.util.URL;
 import com.ruisilva.workshopmongodb.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,5 +23,13 @@ public class PostResource {
         Post obj = postService.findById(id);
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = postService.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 }
