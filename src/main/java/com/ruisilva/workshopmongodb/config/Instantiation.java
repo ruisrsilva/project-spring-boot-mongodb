23a69eedd821ac0ce6563f175b77a872e03b7f13
Rui Silva <ruisrsilva@gmail.com>
2020-10-20 14:01:24 +0100

    Instantiation to database

---
+++ src/main/java/com/ruisilva/workshopmongodb/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.ruisilva.workshopmongodb.config;
+
+import com.ruisilva.workshopmongodb.domain.User;
+import com.ruisilva.workshopmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com");
+        User u3 = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(u1,u2,u3));
+    }
+}
