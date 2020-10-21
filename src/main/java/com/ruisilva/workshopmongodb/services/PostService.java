bed8f5daa6dd5210e2b80a2136f52928c85c9895
Rui Silva <ruisrsilva@gmail.com>
2020-10-21 11:51:30 +0100

    Query with multiple criteria

--- src/main/java/com/ruisilva/workshopmongodb/services/PostService.java
+++ src/main/java/com/ruisilva/workshopmongodb/services/PostService.java
@@ -9,6 +9,7 @@ import com.ruisilva.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,12 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        // adding 24h equivalent of milliseconds to max date
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
